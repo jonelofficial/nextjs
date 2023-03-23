d8ba1441cad78faecf679eaeddb3a3b3bf946055
jonel <it.jonelfignacio@gmail.com>
2023-03-23 13:43:16 +0800

    done routing

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,27 @@
+import Link from "next/link";
+
 const ClientsPage = () => {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "jonel", name: "Jonel" },
+  ];
+
   return (
     <div>
       <h1>Clients Page</h1>
+      <ul>
+        {clients.map((client) => {
+          return (
+            <li key={client.id}>
+              <Link
+                href={{ pathname: "/clients/[id]", query: { id: client.id } }}
+              >
+                {client.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 const Homepage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/blog/test">Blog</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
     </div>
   );
 };
