e229593d3879fc10b4daaa90f2704f218d23af23
jonel <it.jonelfignacio@gmail.com>
2023-03-28 09:11:04 +0800

    initial commit for pre-rendering

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,5 @@
-import EventList from "@/components/events/event-list";
-import { getFeaturedEvents } from "@/dummy-data";
-
 const HomePage = () => {
-  const featuredEvents = getFeaturedEvents();
-
-  return (
-    <div>
-      <EventList items={featuredEvents} />
-    </div>
-  );
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
