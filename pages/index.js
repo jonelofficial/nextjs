55084c27b8055cded377e09b6cc2e0cb101edd80
jonel <it.jonelfignacio@gmail.com>
2023-03-29 09:34:58 +0800

    test

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { getFeaturedEvents } from "@/dummy-data";
 
 const HomePage = () => {
   const featuredEvents = getFeaturedEvents();
-
+  console.log("test");
   return (
     <div>
       <EventList items={featuredEvents} />
